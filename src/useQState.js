6eb5bf2bae7c4f560fb10fc9127de43766013ab6
Shaoqing Tan <tansq7@gmail.com>
2021-04-23 23:25:39 -0400

    Add missing file useQState

---
+++ src/useQState.js
@@ -0,0 +1,57 @@
+import { useState, useCallback } from "react";
+import qs from "query-string";
+import Msgpack from "msgpack-lite";
+const Base65536 = require("base65536");
+
+function uniencode(obj) {
+  return Base65536.encode(Msgpack.encode(obj));
+}
+
+function unidecode(str) {
+  return Msgpack.decode(Base65536.decode(str));
+}
+
+// let o = {foo:'bar',baz:[1,2,3,4,5],quux:'corge'};
+// console.log(uniencode(o).length,JSON.stringify(o).length); // 24, 46
+// console.log(uniencode(o))
+
+const setQueryStringWithoutPageReload = (qsValue) => {
+  const newurl =
+    window.location.protocol +
+    "//" +
+    window.location.host +
+    window.location.pathname +
+    qsValue;
+
+  window.history.pushState({ path: newurl }, "", newurl);
+};
+
+const setQueryStringValue = (
+  key,
+  value,
+  queryString = window.location.search
+) => {
+  const values = qs.parse(queryString);
+  const newQsValue = qs.stringify({ ...values, [key]: uniencode(value) });
+  setQueryStringWithoutPageReload(`?${newQsValue}`);
+};
+
+const getQueryStringValue = (key, queryString = window.location.search) => {
+  const values = qs.parse(queryString);
+  return values[key] ? unidecode(values[key]) : values[key];
+};
+
+function useQState(key, initialValue) {
+  const [value, setValue] = useState(getQueryStringValue(key) || initialValue);
+  const onSetValue = useCallback(
+    (newValue) => {
+      setValue(newValue);
+      setQueryStringValue(key, newValue);
+    },
+    [key]
+  );
+
+  return [value, onSetValue];
+}
+
+export default useQState;
